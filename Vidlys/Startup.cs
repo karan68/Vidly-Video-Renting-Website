4efae645686801ff436e1248d62c302975eb3777
karan yadav <47856276+karan68@users.noreply.github.com>
2021-03-14 22:40:03 +0530

    Add files via upload

---
+++ Vidlys/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidlys.Startup))]
+namespace Vidlys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           
+            ConfigureAuth(app);
+        }
+    }
+}
