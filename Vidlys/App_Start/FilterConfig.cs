6d9a99e6598c79465be13b1256a0656e43b9a685
karan yadav <47856276+karan68@users.noreply.github.com>
2021-03-14 22:29:12 +0530

    Add files via upload

---
+++ Vidlys/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vidlys
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute()); //applying the authorize action globally
+            filters.Add(new RequireHttpsAttribute()); //the app will only be opened by https channel
+        }
+    }
+}
